1dd872c35773d53550843e9a549be496191a0e11
Nat Rurka <rurkan@oregonstate.edu>
2026-09-16 12:02:20 -0700

    Improved OS compatability for install and run scripts

--- install.sh
+++ install.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 
-# IF LINUX:
-# if [ "$(uname)" == "Linux" ]; then
-python3 -m venv .venv
-source .venv/bin/activate
-# fi
-
 printf "Beginning installation\n"
-# python3 -m venv venv
-# Cleanup of any previous install
+
 printf "Cleaning up old installations\n"
-rm -f jsonFormat_pb2*
+rm -rf .venv
+# rm -f jsonFormat_pb2*
 rm -rf logs
+printf "This script does not clean the local_data folder automatically\n"
+
+PYTHON_VER="python"
+PIP_VER="pip"
+
+if command -v python3 >/dev/null 2>&1; then
+  PYTHON_VER="python3"
+  PIP_VER="pip3"
+elif command -v python >/dev/null 2>&1; then
+  PYTHON_VER="python"
+  PIP_VER="pip"
+elif command -v py >/dev/null 2>&1; then
+  PYTHON_VER="py"
+  PIP_VER="py -m pip"
+else
+  printf "Installation incomplete, no python found.\n"
+  exit(0)
+fi
+
+$PYTHON_VER -m venv .venv
+if [ "$(uname)" == "Linux" ]; then
+  source .venv/bin/activate
+elif [ $OSTYPE == "msys" ]; then
+  source .venv/Scripts/activate
+fi
 
 printf "Creating ./logs folder and ./logs/pip.log file\n"
 mkdir logs
@@ -22,8 +41,9 @@ echo "username,password_plaintext" > local_data/users_plaintext.csv
 touch local_data/user_info.csv
 mkdir local_data/puzzle_history
 
+printf "Installing required packages with pip\n"
 # pip3 install grpcio grpcio-tools chess > logs/pip.log
-pip3 install chess pandas > logs/pip.log
+$PIP_VER install chess pandas > logs/pip.log
 cd src
 # python3 -m grpc_tools.protoc -I ../proto --python_out=. --grpc_python_out=. ../proto/jsonFormat.proto
 cd ..
--- run.sh
+++ run.sh
@@ -5,6 +5,22 @@ if ! [ -d "./.venv" ]; then
     ./install.sh
 fi
 
+PYTHON_VER="python"
 
-VENV_PYTHON="./.venv/bin/python3"
-$VENV_PYTHON src/init.py
+if command -v python3 >/dev/null 2>&1; then
+  PYTHON_VER="python3"
+elif command -v python >/dev/null 2>&1; then
+  PYTHON_VER="python"
+elif command -v py >/dev/null 2>&1; then
+  PYTHON_VER="py"
+else
+  printf "Python was not found, please install python.\n"
+fi
+
+if [ "$(uname)" == "Linux" ]; then
+  source .venv/bin/activate
+elif [ $OSTYPE == "msys" ]; then
+  source .venv/Scripts/activate
+fi
+
+$PYTHON_VER src/init.py
